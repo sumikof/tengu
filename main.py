ab0c1b49a1d5b1e23aecd276d5e71395c06b8884
sumiya <sumikof187@gmail.com>
2020-03-29 00:03:48 +0900

    高値,安値をgraph描画

--- main.py
+++ main.py
@@ -14,18 +14,37 @@ def rest_action(account):
     action = oanda_rest_json()
     oanda_rest_api(account, instrument="USD_JPY", action=action.printdump, count=100, granularity="M1")
 
+def create_ex_csv(account):
+    from oanda_action.create_csv import create_csv
+    create_csv(account,instrument="USD_JPY",from_date="2020-01-01T00:00:00")
+
 def get_account():
     from oanda_api.account import create_account
     account = create_account('oanda.conf')
     return account
 
-def create_ex_csv(account):
-    from oanda_action.create_csv import create_csv
-    create_csv(account,instrument="USD_JPY",from_date="2020-01-01T00:00:00")
+def oanda_dataframe():
+    import pandas as pd
+    df = pd.read_csv('USD_JPY_M1.csv')
+    df['date'] = pd.to_datetime(df['date'])
+    #df = df[df['date'].dt.minute % 10 == 0]
+    df = df.tail(1000)
+
+    return df
+
+
+def oanda_graph(df):
+    import matplotlib.pyplot as plt
+
+    plt.plot(df['date'], df['high'],color = 'red')
+    plt.plot(df['date'], df['low'], color='blue')
+    plt.show()
+
 
 def main():
-    rest_action(get_account())
-    create_ex_csv(get_account())
+    df = oanda_dataframe()
+    print(df)
+    oanda_graph(df)
 
 
 
